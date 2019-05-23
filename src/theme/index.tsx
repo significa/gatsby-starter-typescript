a67f3987c298d476671694b3e530d91653a09e08
Danilo Woznica <danilowoz@gmail.com>
2019-05-23 14:00:47 +0100

    fix(theme): remove condicional theme

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -11,44 +11,23 @@ import { media } from './breakpoints'
 import style from './style'
 import theme from './theme'
 
-import { ITheme } from './types'
-
-/** Theme Context */
-/* tslint:disable:no-empty */
-const {
-  Provider: ThemeContextProvider,
-  Consumer: ThemeConsumer,
-} = React.createContext({ theme: {}, updateTheme: (_: ITheme): void => {} })
-/* tslint:enable:no-empty */
-
 /** Global styles */
 const GlobalStyle = createGlobalStyle`
   ${style}
 `
 
 /** Theme Provider */
-class ThemeProvider extends React.Component<
-  { children: React.ReactNode },
-  { theme: ITheme }
-> {
-  state = { theme }
-
-  updateTheme = (t: ITheme): void => this.setState({ theme: t })
-
-  render() {
-    return (
-      <ThemeContextProvider value={{ theme, updateTheme: this.updateTheme }}>
-        <StyledThemeProvider theme={this.state.theme}>
-          <>
-            <GlobalStyle />
-            {this.props.children}
-          </>
-        </StyledThemeProvider>
-      </ThemeContextProvider>
-    )
-  }
+const ThemeProvider: React.FC = ({ children }) => {
+  return (
+    <StyledThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
+        {children}
+      </>
+    </StyledThemeProvider>
+  )
 }
 
-export { theme, ThemeProvider, ThemeConsumer, keyframes, css, media }
+export { theme, ThemeProvider, keyframes, css, media }
 
 export default styled
